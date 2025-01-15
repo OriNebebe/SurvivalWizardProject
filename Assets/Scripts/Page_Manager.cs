00be79958e5a9a984c6faf904f5c710f497b327a
Mimo <137498544+mimakuma@users.noreply.github.com>
2025-01-15 23:56:12 +0100

    More work on Bunker's UI
    It's gonna be coded properly later, for now it's just for show and placeholding, sowwy guys >w<

---
+++ Assets/Scripts/Page_Manager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page_Manager : MonoBehaviour
+{
+    [SerializeField] TMPro.TextContainer PageTitle;
+    [SerializeField] TMPro.TextContainer PageContents;
+
+
+}
---
+++ Assets/Scripts/Stupid_UI_Debug.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stupid_UI_Debug : MonoBehaviour
+{
+
+    [Header("Pages Panels")]
+    [SerializeField] GameObject RaportPanel;
+    [SerializeField] GameObject ResourcesPanel;
+    [SerializeField] GameObject TeamPanel;
+    [SerializeField] GameObject EventsPanel;
+    [SerializeField] GameObject MapPanel;
+    [SerializeField] GameObject UpgradesPanel;
+    [SerializeField] GameObject SettingsPanel;
+
+    private void Start()
+    {
+        
+    }
+
+}
